a9cb8dc42a2320a985873592df9cc16f167515a5
DTUVIRENDRA <97440846+DTUVIRENDRA@users.noreply.github.com>
2022-01-18 18:33:29 +0100

    Add files via upload

---
+++ noisecorrect_jan12a.ino
@@ -0,0 +1,49 @@
+#include <ESP8266WiFi.h>
+#include <ThingSpeak.h>
+const char* ssid = "VPRIME";
+const char* pass = "virendra";
+WiFiClient client;
+
+unsigned long channelID = 1633876; //your TS channal
+const char * APIKey = "8AKFKWP4151FANXM"; //your TS API
+const char* server = "api.thingspeak.com";
+const int postDelay = 1000; //post data
+
+int ledpin=13; 
+int soundpin=A0;
+int soundpinDigital=2;
+int threshold=200;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600); //sensor baud rate
+  WiFi.begin(ssid, pass);
+  pinMode(ledpin, OUTPUT); // led pin connected to D7
+  pinMode(soundpin, INPUT);// analog pin connected to A0 
+  pinMode(soundpinDigital, INPUT); // digital pin connected to 2 
+}
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int soundsense=analogRead(soundpin);
+ // int soundsense=digitalRead(soundpin); //Noise level sensor output pin 
+    ThingSpeak.begin(client);
+  client.connect(server, 80); //connect(URL, Port)
+  ThingSpeak.setField(1, soundsense); //set data on the X graph
+  ThingSpeak.writeFields(channelID, APIKey);//post everything to TS
+  client.stop();
+  Serial.print("Analog data is: "); // print as Analog data is 
+  Serial.print(soundsense);
+  Serial.println();
+ 
+
+ if (soundsense>=threshold){
+    digitalWrite(ledpin, HIGH); // led is on
+  }
+  else{
+    digitalWrite(ledpin, LOW); // led is off
+  }
+ delay(postDelay); //wait and then post again
+
+}
